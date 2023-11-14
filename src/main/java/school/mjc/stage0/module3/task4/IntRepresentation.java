bdaee95bd8ad5cf7c2761c966998c8afb6b3ad20
afdansx <dd.anidenko@gmail.com>
2023-11-14 23:57:42 +0200

    solved

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+
+        int int1 = c;
+        int int2 = h;
+        int int3 = a;
+        int int4 = r;
+        System.out.println(int1);
+        System.out.println(int2);
+        System.out.println(int3);
+        System.out.println(int4);
     }
 }
